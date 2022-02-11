e1a6085934ea5f56c0daac444d4d145188071a7e
Velmurugan Moorthy <vel@Vels-MacBook-Pro.local>
2022-02-11 23:33:14 +0530

    Vel | LinkedList - Insert @ start

--- src/com/practice/ds/linkedlist/LinkedListDriver.java
+++ src/com/practice/ds/linkedlist/LinkedListDriver.java
@@ -1,14 +1,21 @@
 package com.practice.ds.linkedlist;
 
 
-public class LinkedListPrintDriver {
+public class LinkedListDriver {
     public static void main(String[] args) {
 
         LinkedListNode head = populateLinkedListHead();
+        LinkedListNode firstNode = new LinkedListNode(0);
 
+        //Exected output : 1 -> 2 -> 3 -> 4
         LinkedList linkedList = new LinkedList();
         linkedList.printLinkedList(head);
 
+        //Exected output : 0 -> 1 -> 2 -> 3 -> 4
+        // (Refer line #10 for prev state of LinkedList)
+        LinkedListNode result = linkedList.insertAtStart(head, 0);
+        System.out.println("\n LinkedList after insertion @ start");
+        linkedList.printLinkedList(result);
     }
 
     private static LinkedListNode populateLinkedListHead() {
