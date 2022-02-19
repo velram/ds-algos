a8379a59f703cd2aeebc8262c0ef2e6037112667
Velmurugan Moorthy <vel@Vels-MacBook-Pro.local>
2022-02-19 10:18:48 +0530

    Vel - LinkedList - delete from Kth Last location

---
+++ src/com/practice/ds/linkedlist/LinkedListRemoveFromLastDriver.java
@@ -0,0 +1,32 @@
+package com.practice.ds.linkedlist;
+
+/**
+ * @author Velmurugan
+ * @date 18.02.2022
+ *
+ */
+public class LinkedListRemoveFromLastDriver {
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+
+    }
+}
+
+class LinkedListLastNodeRemovalDriver {
+
+    /**
+     * Approach :
+     *  - Traverse thru the entire list
+     *  - count the list length
+     *  - 
+     * @param head
+     * @param kthLastIndex
+     * @return
+     */
+    public LinkedListNode removeKthLastNode(LinkedListNode head, int kthLastIndex){
+
+
+        return null;
+    }
+}
